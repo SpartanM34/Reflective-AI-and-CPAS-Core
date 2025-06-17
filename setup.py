1a5f508a7055c13402faacc366e859a3e68d9198
SpartanM34 <116921514+SpartanM34@users.noreply.github.com>
2025-06-16 22:57:34 -0400

    Add dependencies and bump version

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="cpas_autogen",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(include=["cpas_autogen", "cpas_autogen.*"]),
     description="CPAS AutoGen utilities",
-    install_requires=[],
+    install_requires=[
+        "flask",
+        "numpy",
+        "sentence-transformers",
+    ],
 )
